65a7d8108dc95d0723a28eb3ccb7d08aad18ec48
hai.nguyen <15892244+nduyhai@users.noreply.github.com>
2024-10-30 14:45:04 +0700

    feat: add script init product

---
+++ product-service/src/main/resources/db/migration/V3__add_mac_mini.sql
@@ -0,0 +1,7 @@
+insert into products
+values ('f7a2366d-26fb-4263-a236-6d26fba26372', 'Mac14,3',
+        '10-Core CPU, 10-Core GPU, 24GB Unified Memory, 512GB SSD Storage Footnote', 'Mac mini M4',
+        '2bf19bac-6983-4770-b19b-ac6983c770e0', now(), now()),
+       ('c6c136e9-362b-4ff3-8136-e9362bfff3ff', 'Mac14,3',
+        '12-Core CPU, 16-Core GPU, 24GB Unified Memory, 512GB SSD Storage Footnote',
+        'Mac mini M4 Pro', '2ee94a9b-5d29-4aa4-a94a-9b5d29faa4ab', now(), now());
